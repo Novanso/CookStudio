1e214818a6e99aedf8c03039d30c4c50e0682e3c
Novanso <nathanael.v@outlook.fr>
2025-06-04 14:01:58 +0200

    Admin simplification

--- web/src/components/Administration.js
+++ web/src/components/Administration.js
@@ -23,91 +23,40 @@ const Administration = ({ authToken }) => {
       }
     };
     fetchUser();
-    document.getElementById('userstitle').style.setProperty("background-color", "var(--dark-tertiary)");
   }, [authToken]);
 
   const handleActivepage = (toSet) => {
     setActivePage(toSet)
-    switch(toSet) {
-      case 'Users':
-        document.getElementById('userstitle').style.setProperty("background-color", "var(--dark-tertiary)")
-        document.getElementById('tagstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('catagoriestitle').style.setProperty("background-color", "inherit")
-        document.getElementById('externaltitle').style.setProperty("background-color", "inherit")
-        document.getElementById('ingredientstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('statisticstitle').style.setProperty("background-color", "inherit")
-        break;
-      case 'Tags':
-        document.getElementById('userstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('tagstitle').style.setProperty("background-color", "var(--dark-tertiary)")
-        document.getElementById('catagoriestitle').style.setProperty("background-color", "inherit")
-        document.getElementById('externaltitle').style.setProperty("background-color", "inherit")
-        document.getElementById('ingredientstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('statisticstitle').style.setProperty("background-color", "inherit")
-        break;
-      case 'Categories':
-        document.getElementById('userstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('tagstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('catagoriestitle').style.setProperty("background-color", "var(--dark-tertiary)")
-        document.getElementById('externaltitle').style.setProperty("background-color", "inherit")
-        document.getElementById('ingredientstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('statisticstitle').style.setProperty("background-color", "inherit")
-        break;
-      case 'External':
-        document.getElementById('userstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('tagstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('catagoriestitle').style.setProperty("background-color", "inherit")
-        document.getElementById('externaltitle').style.setProperty("background-color", "var(--dark-tertiary)")
-        document.getElementById('ingredientstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('statisticstitle').style.setProperty("background-color", "inherit")
-        break;
-      case 'Ingredients':
-        document.getElementById('userstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('tagstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('catagoriestitle').style.setProperty("background-color", "inherit")
-        document.getElementById('externaltitle').style.setProperty("background-color", "inherit")
-        document.getElementById('ingredientstitle').style.setProperty("background-color", "var(--dark-tertiary)")
-        document.getElementById('statisticstitle').style.setProperty("background-color", "inherit")
-        break;
-      case 'Statistics':
-        document.getElementById('userstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('tagstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('catagoriestitle').style.setProperty("background-color", "inherit")
-        document.getElementById('externaltitle').style.setProperty("background-color", "inherit")
-        document.getElementById('ingredientstitle').style.setProperty("background-color", "inherit")
-        document.getElementById('statisticstitle').style.setProperty("background-color", "var(--dark-tertiary)")
-        break;
-    }
-  };
-  
+  }
+
+  function Page({ name, isActive }) {
+  return (
+    <div id={{name}} onClick={() => handleActivepage(name)} className={isActive ? 'active-admin-button' : 'admin-button'}>
+      { name === 'users' && <div>{ texts.users }</div>}
+      { name === 'tags' && <div>Handle Tags</div>}
+      { name === 'categories' && <div>Handle Categories</div>}
+      { name === 'external' && <div>External Access</div>}
+      { name === 'ingredients' && <div>{ texts.ingredients }</div>}
+      { name === 'statistics' && <div>{ texts.stats }</div>}
+    </div>
+  );
+}  
   return (
     <div className="settings-container"> 
       <div className="SectionLeft">
-        <div className='settings-button' id='userstitle' onClick={() => handleActivepage(('Users'))}>
-        <div>{ texts.users }</div>
-        </div>
-        <div className='settings-button' id='tagstitle' onClick={() => handleActivepage('Tags')}>
-          <div>Handle Tags</div>
-        </div>
-        <div className='settings-button' id='catagoriestitle' onClick={() => handleActivepage('Categories')}>
-          <div>Handle Categories</div>
-        </div>
-        <div className='settings-button' id='externaltitle' onClick={() => handleActivepage('External')}>
-          <div>External Access</div>
-        </div>
-        <div className='settings-button' id='ingredientstitle' onClick={() => handleActivepage('Ingredients')}>
-          <div>{ texts.ingredients }</div>
-        </div>
-        <div className='settings-button' id='statisticstitle' onClick={() => handleActivepage('Statistics')}>
-          <div>{ texts.stats }</div>
-        </div>
+        <Page isActive={activePage === 'users' && true} name="users"></Page>
+        <Page isActive={activePage === 'tags' && true} name="tags"></Page>
+        <Page isActive={activePage === 'categories' && true} name="categories"></Page>
+        <Page isActive={activePage === 'external' && true} name="external"></Page>
+        <Page isActive={activePage === 'ingredients' && true} name="ingredients"></Page>
+        <Page isActive={activePage === 'statistics' && true} name="statistics"></Page>
       </div>
       <div className="SectionRight"> 
-        { activePage === 'Ingredients' && (
-            <Ingredients authToken={authToken} />
-        ) }
-        { activePage === 'Users' && (
+        { activePage === 'users' && (
           <Users authToken={authToken} />
+        ) }
+        { activePage === 'ingredients' && (
+            <Ingredients authToken={authToken} />
         ) }
       </div>
     </div>
